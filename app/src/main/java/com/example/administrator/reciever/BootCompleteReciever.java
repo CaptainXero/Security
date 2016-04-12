915fbb82ab35c0620bfca985c84e5b5fb1ef390c
ZhaoYi <297756521@qq.com>
2016-04-12 20:35:28 +0800

    完成流量统计模块

--- app/src/main/java/com/example/administrator/reciever/BootCompleteReciever.java
+++ app/src/main/java/com/example/administrator/reciever/BootCompleteReciever.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.example.administrator.action.App;
+import com.example.administrator.service.TrafficMonitoringService;
+import com.example.administrator.utils.SystemInfoUtils;
 
 public class BootCompleteReciever extends BroadcastReceiver {
     private static final String TAG = BootCompleteReciever.class.getSimpleName();
@@ -13,8 +15,11 @@ public class BootCompleteReciever extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+        if(SystemInfoUtils.isServiceRunning(context,
+                "com.example.administrator.service.TrafficMonitoringService")){
+            //开启服务
+            context.startService(new Intent(context, TrafficMonitoringService.class));
+        }
         ((App) context.getApplicationContext()).correctSIM();
     }
 }
